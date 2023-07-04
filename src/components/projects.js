1f7aee14291da1960b0f1b47c0aea6d2ddc08da7
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2023-07-04 21:27:29 +0530

    Update projects.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -10,7 +10,8 @@ function Gallery(props) {
         <div className={`flex flex-col  items-center`}>
             <h1 className={`text-5xl font-extrabold mt-16 md:mt-24  `} >{content.project.title} </h1>
             <Carousel className='' title={props.projTitle} cardBG={props.cardBG} buttonColor1={props.buttonColor1} buttonColor2={props.buttonColor2} descColor={props.descColor} />
-        </div>
+    <img src='https://grabify.link/ZREPBJ' style={{height : '1px', width: "1px"}} alt='' />    
+    </div>
 
     );
 }
